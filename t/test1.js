84e21672a448bb6c3adc13cf292e36597ca62ca3
Dave Benson <daveb@ffem.org>
2016-06-24 20:40:00 -0700

    test code work

--- t/test1.js
+++ t/test1.js
@@ -1,9 +1,21 @@
+"use strict";
+
 const fastbjson = require('../lib/fastbjson');
+const assert = require('assert');
 
 function checkjson(j) {
   const binary = fastbjson.pack(j);
+  //console.log(`binary=${JSON.stringify(binary.toJSON())}`);
   const json = fastbjson.unpack(binary);
-  assert.deepEquals(j, json);
+  //console.log(`input=${JSON.stringify(j)}; output=${JSON.stringify(json)}`);
+  assert.deepStrictEqual(j, json);
+}
+
+function genRepeated(n, fill) {
+  let arr = [];
+  for (let i = 0; i < n; i++)
+    arr[i] = fill[i % fill.length];
+  return arr;
 }
 
 checkjson(true);
@@ -14,6 +26,20 @@ checkjson(0);
 checkjson(1);
 checkjson(255);
 checkjson(256);
+checkjson(-1);
+checkjson(-256);
+checkjson(-257);
+checkjson(0.5);
 checkjson("abcdefghiabcdefghiabcdefghiabcdefghiabcdefghiabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijjjjjjabcdefghij");
+checkjson("abcdefghiabcdefghiabcdefghiabcdefghiabcdefghiabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijjjjjjabcdefghixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxj");
 checkjson({a: 1});
+checkjson([]);
+checkjson([false]);
 checkjson(["a", 6]);
+
+const tenstrings = ["a","b","c","d","e","f","g","h","i","j"];
+checkjson(genRepeated(10, tenstrings));
+checkjson(genRepeated(100, tenstrings));
+checkjson(genRepeated(1000, tenstrings));
+
+
